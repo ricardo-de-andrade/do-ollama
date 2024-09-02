6f70b0bb944d2a26c14d95de33225dfc008a0658
Ricardo de Andrade <rdeandrade@google.com>
2024-09-02 12:06:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -25,23 +25,13 @@ def process_form():
 
     print("User:", message)
 
-    # prompt = '''
-    # You are a nice and helpful chatbot.
-    # Be succint and to the point, unless the user asks for more details.
-    # Answer the user's question, taking in consideration the conversation history.
-
-    # ***QUESTION***
-    # ''' + message + '''
-
-    # ***HISTORY***
-    # ''' + str(chat_history)
-
-    # print("Prompt: ",prompt)
     payload = {
         "model": "llama3.1",
         "messages": [],
         "stream": False
     }
+
+    # Build the history for API call
     # chat_history : [(user_time, user_input, response_time, bot_text),...]
     for msg in chat_history:
         user_msg = {
@@ -54,31 +44,19 @@ def process_form():
             "content": msg[3]
         }
         payload['messages'].append(bot_msg)
+
+    # Add latest request
     payload['messages'].append(
         {
             "role":"user",
             "content":message
         }
     )
-    
-    print(payload)
+
     response = requests.post('http://localhost:11434/api/chat',
-                            #  json={
-                            #     "model": "llama3.1", 
-                            #     "prompt": prompt, 
-                            #     "stream":False},
                              json=payload,
                              headers={'Content-Type': 'application/json'})
-# {
-#   "model":"llama3.1", 
-#   "messages":[{
-#        "role":"user",
-#        "content":"i will call you jonny"
-#      },{
-#        "role":"assistant",
-#        "content":"ok, i like that"
-#      },{"role":"user","content":"who is jonny?"}], "stream":false}   
-    print(response.text)                    
+ 
     bot_text = response.json()['message']['content']
     print("Bot:",bot_text)
     bot_text = md.markdown(bot_text)
