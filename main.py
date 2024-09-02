fd5fef02bb64bca637ae00fe3f668bf4b261825b
Ricardo de Andrade <rdeandrade@google.com>
2024-09-02 11:56:38 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -25,23 +25,61 @@ def process_form():
 
     print("User:", message)
 
-    prompt = '''
-    You are a nice and helpful chatbot.
-    Be succint and to the point, unless the user asks for more details.
-    Answer the user's question, taking in consideration the conversation history.
+    # prompt = '''
+    # You are a nice and helpful chatbot.
+    # Be succint and to the point, unless the user asks for more details.
+    # Answer the user's question, taking in consideration the conversation history.
 
-    ***QUESTION***
-    ''' + message + '''
+    # ***QUESTION***
+    # ''' + message + '''
 
-    ***HISTORY***
-    ''' + str(chat_history)
+    # ***HISTORY***
+    # ''' + str(chat_history)
 
-    print("Prompt: ",prompt)
-    response = requests.post('http://localhost:11434/api/generate',
-                             json={"model": "llama3.1", "prompt": prompt, "stream":False},
+    # print("Prompt: ",prompt)
+    payload = {
+        "model": "llama3.1",
+        "messages": [],
+        "stream": False
+    }
+    # chat_history : [(user_time, user_input, response_time, bot_text),...]
+    for msg in chat_history:
+        user_msg = {
+            "role": "user",
+            "content": msg[1]
+        }
+        payload['messages'].append(user_msg)
+        bot_msg = {
+            "role": "assistant",
+            "content": msg[3]
+        }
+        payload['messages'].append(bot_msg)
+    payload['messages'].append(
+        {
+            "role":"user",
+            "content":message
+        }
+    )
+    
+    print(payload)
+    response = requests.post('http://localhost:11434/api/chat',
+                            #  json={
+                            #     "model": "llama3.1", 
+                            #     "prompt": prompt, 
+                            #     "stream":False},
+                             json=payload,
                              headers={'Content-Type': 'application/json'})
-                       
-    bot_text = response.json()['response']
+# {
+#   "model":"llama3.1", 
+#   "messages":[{
+#        "role":"user",
+#        "content":"i will call you jonny"
+#      },{
+#        "role":"assistant",
+#        "content":"ok, i like that"
+#      },{"role":"user","content":"who is jonny?"}], "stream":false}   
+    print(response.text)                    
+    bot_text = response.json()['message']['content']
     print("Bot:",bot_text)
     bot_text = md.markdown(bot_text)
 
@@ -53,4 +91,4 @@ def process_form():
     return render_template('chat.html', history=chat_history)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', port='8080', debug=True)
